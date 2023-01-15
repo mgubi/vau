6502088fd4b20561411a878e0ad776ba25ba7a37
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2023-01-15 22:26:59 +0000

    More fixes...

--- src/Vau/vau_main.cpp
+++ src/Vau/vau_main.cpp
@@ -10,12 +10,35 @@
 #include "vau_buffer.hpp"
 #include "vau_editor.hpp"
 #include "file.hpp"
+#include "merge_sort.hpp"
+#include "drd_std.hpp"
+#include "convert.hpp"
+#include "boot.hpp"
+
+/******************************************************************************
+* Subroutines for paths
+******************************************************************************/
+
+static url
+get_env_path (string which) {
+  return url ("$" * which);
+}
 
 static void
 set_env_path (string which, url val) {
-  cout << which << " := " << val << "\n";
+  //cout << which << " := " << val << "\n";
   if ((!is_none (val)) && (val->t != ""))
     set_env (which, as_string (val));
+}
+
+static url
+get_env_path (string which, url def) {
+  url val= get_env_path (which);
+  if (is_none (val) || (val->t == "")) {
+    set_env_path (which, def);
+    return def;
+  }
+  return val;
 }
 
 static void
@@ -65,10 +88,115 @@ init_user_dirs () {
   //clean_temp_dirs ();
 }
 
+static url
+plugin_path (string which) {
+  url base= "$TEXMACS_HOME_PATH:/etc/TeXmacs:$TEXMACS_PATH:/usr/share/TeXmacs";
+  url search= base * "plugins" * url_wildcard ("*") * which;
+  return expand (complete (search, "r"));
+}
+
+scheme_tree
+plugin_list () {
+  bool flag;
+  array<string> a= read_directory ("$TEXMACS_PATH/plugins", flag);
+  a << read_directory ("/etc/TeXmacs/plugins", flag);
+  a << read_directory ("$TEXMACS_HOME_PATH/plugins", flag);
+  a << read_directory ("/usr/share/TeXmacs/plugins", flag);
+  merge_sort (a);
+  int i, n= N(a);
+  tree t (TUPLE);
+  for (i=0; i<n; i++)
+    if ((a[i] != ".") && (a[i] != "..") && ((i==0) || (a[i] != a[i-1])))
+      t << a[i];
+  return t;
+}
+
+
+/******************************************************************************
+* Set additional environment variables
+******************************************************************************/
+
+static void
+init_env_vars () {
+  // Handle binary, library and guile paths for plugins
+  url bin_path= get_env_path ("PATH"); // | plugin_path ("bin");
+#if defined (OS_MINGW) || defined (OS_MACOS)
+  bin_path= bin_path | url ("$TEXMACS_PATH/bin");
+//  if (has_user_preference ("manual path"))
+//    bin_path= url_system (get_user_preference ("manual path")) | bin_path;
+#endif
+
+  set_env_path ("PATH", bin_path);
+  url lib_path= get_env_path ("LD_LIBRARY_PATH") | plugin_path ("lib");
+  set_env_path ("LD_LIBRARY_PATH", lib_path);
+
+  // Get TeXmacs style and package paths
+  url style_root=
+    get_env_path ("TEXMACS_STYLE_ROOT",
+                  "$TEXMACS_HOME_PATH/styles:$TEXMACS_PATH/styles" | plugin_path ("styles"));
+  url package_root=
+    get_env_path ("TEXMACS_PACKAGE_ROOT",
+                  "$TEXMACS_HOME_PATH/packages:$TEXMACS_PATH/packages" |
+                  plugin_path ("packages"));
+  url all_root= style_root | package_root;
+  url style_path=
+    get_env_path ("TEXMACS_STYLE_PATH",
+                  search_sub_dirs (all_root));
+  url text_root=
+    get_env_path ("TEXMACS_TEXT_ROOT",
+                  "$TEXMACS_HOME_PATH/texts:$TEXMACS_PATH/texts" |
+                  plugin_path ("texts"));
+  url text_path=
+    get_env_path ("TEXMACS_TEXT_PATH",
+                  search_sub_dirs (text_root));
+
+  // Get other data paths
+  (void) get_env_path ("TEXMACS_FILE_PATH",text_path | style_path);
+  (void) set_env_path ("TEXMACS_DOC_PATH",
+                       get_env_path ("TEXMACS_DOC_PATH") |
+                       "$TEXMACS_HOME_PATH/doc:$TEXMACS_PATH/doc" |
+                       plugin_path ("doc"));
+  (void) set_env_path ("TEXMACS_SECURE_PATH",
+                       get_env_path ("TEXMACS_SECURE_PATH") |
+                       "$TEXMACS_PATH:$TEXMACS_HOME_PATH");
+  (void) get_env_path ("TEXMACS_PATTERN_PATH",
+                       "$TEXMACS_HOME_PATH/misc/patterns" |
+                       url ("$TEXMACS_PATH/misc/patterns") |
+                       url ("$TEXMACS_PATH/misc/pictures") |
+                       plugin_path ("misc/patterns"));
+  (void) get_env_path ("TEXMACS_PIXMAP_PATH",
+                       "$TEXMACS_HOME_PATH/misc/pixmaps" |
+                       url ("$TEXMACS_PATH/misc/pixmaps/modern/32x32/settings") |
+                       url ("$TEXMACS_PATH/misc/pixmaps/modern/32x32/table") |
+                       url ("$TEXMACS_PATH/misc/pixmaps/modern/24x24/main") |
+                       url ("$TEXMACS_PATH/misc/pixmaps/modern/20x20/mode") |
+                       url ("$TEXMACS_PATH/misc/pixmaps/modern/16x16/focus") |
+                       url ("$TEXMACS_PATH/misc/pixmaps/traditional/--x17") |
+                       plugin_path ("misc/pixmaps"));
+  (void) get_env_path ("TEXMACS_DIC_PATH",
+                       "$TEXMACS_HOME_PATH/langs/natural/dic" |
+                       url ("$TEXMACS_PATH/langs/natural/dic") |
+                       plugin_path ("langs/natural/dic"));
+  (void) get_env_path ("TEXMACS_THEME_PATH",
+                       url ("$TEXMACS_PATH/misc/themes") |
+                       url ("$TEXMACS_HOME_PATH/misc/themes") |
+                       plugin_path ("misc/themes"));
+#ifdef OS_WIN32
+  set_env ("TEXMACS_SOURCE_PATH", "");
+#else
+  set_env ("TEXMACS_SOURCE_PATH", TEXMACS_SOURCES);
+#endif
+}
 
 void
 TeXmacs_main (int argc, char** argv) {
   
+  init_std_drd ();
+  //cout << "Initialize -- User preferences\n";
+  load_user_preferences ();
+  //cout << "Initialize -- Environment variables\n";
+  init_env_vars ();
+
   initialize_scheme ();
   
   string tm_init_file= "/Users/mgubi/t/vau/src/Vau/init-vau.scm";
